e130de288e5cdf795478f9e263ae77d49cb7cdf9
Kevin Alderson <kevin_alderson@yahoo.co.uk>
2024-02-29 21:54:52 +0000

    Modified app.run command. New images and pages added.

--- run.py
+++ run.py
@@ -77,4 +77,5 @@ def donate():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    host = (os.environ.get("HOST"))
+    app.run(host=host, port=port, debug=True)
