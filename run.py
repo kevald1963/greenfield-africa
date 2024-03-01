b4739d5358fa71699a17a4f92c5353e5b607fbb7
Kevin Alderson <kevin_alderson@yahoo.co.uk>
2024-03-01 21:56:42 +0000

    Modified app.run again. Would not work in Heroku, just locally. Host set to 0.0.0.0

--- run.py
+++ run.py
@@ -77,5 +77,5 @@ def donate():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    host = (os.environ.get("HOST"))
+    host = "0.0.0.0"
     app.run(host=host, port=port, debug=True)
